c1bc46ac5b3102101705eea3d192addf0f4d1167
dinyaaal <wdinyaaal@gmail.com>
2025-05-29 06:40:13 +0300

    add deliting photos

--- src/app/[locale]/(with-header)/dashboard/add-advertisement/components/advertisement.tsx
+++ src/app/[locale]/(with-header)/dashboard/add-advertisement/components/advertisement.tsx
@@ -230,6 +230,34 @@ export default function Advertisement({ categories }: SellProps) {
       setPhotos((prevPhotos) => [...prevPhotos, ...Array.from(files)]);
     }
   };
+  const handleDeletePhotos = async () => {
+    setPhotos([]);
+    if (product && session?.user.access_token) {
+      try {
+        const res = await fetch("/api/products/photos/delete", {
+          method: "DELETE",
+          headers: {
+            token: session?.user.access_token,
+            id: String(product.id),
+          },
+        });
+
+        if (!res.ok) {
+          const errData = await res.json();
+          throw new Error("Ошибка при удалении фото");
+        }
+
+        setProduct({
+          ...product,
+          photos: [],
+        });
+      } catch (err) {
+        console.error("Ошибка при удалении:", err);
+        toast.error("Ошибка при удалении фото");
+      }
+    }
+  };
+
   const handleFilesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files) {
@@ -335,11 +363,9 @@ export default function Advertisement({ categories }: SellProps) {
       }
     });
 
-    if (photos && photos.length > 0) {
-      photos.forEach((photo) => {
-        formData.append("photos[]", photo);
-      });
-    }
+    photos.forEach((photo) => {
+      formData.append("photos[]", photo);
+    });
 
     if (files && files.length > 0) {
       files.forEach((file) => {
@@ -807,131 +833,97 @@ export default function Advertisement({ categories }: SellProps) {
                         htmlFor="advertisement-photo"
                         className="input-body-file__input input"
                       >
-                        {/* <Swiper
-                          spaceBetween={20}
-                          slidesPerView={1}
-                          modules={[Navigation]}
-                          className="w-full h-full"
-                          // navigation={{
-                          //   nextEl: ".swiper-arrow-next",
-                          //   prevEl: ".swiper-arrow-prev",
-                          // }}
-                        >
-                          <SwiperSlide>
-                            <Image
-                              src={"/icons/image.svg"}
-                              className="ibg"
-                              alt="User image"
-                              width={600}
-                              height={600}
-                            />
-                          </SwiperSlide>
-                          <SwiperSlide>
-                            <Image
-                              src={"/icons/image.svg"}
-                              className="ibg"
-                              alt="User image"
-                              width={600}
-                              height={600}
-                            />
-                          </SwiperSlide>
-                          <SwiperSlide>
-                            <Image
-                              src={"/icons/image.svg"}
-                              className="ibg"
-                              alt="User image"
-                              width={600}
-                              height={600}
-                            />
-                          </SwiperSlide>
-                          <SwiperSlide>
-                            <Image
-                              src={"/icons/image.svg"}
-                              className="ibg"
-                              alt="User image"
-                              width={600}
-                              height={600}
-                            />
-                          </SwiperSlide>
-                        </Swiper> */}
-                        {photos && photos?.length > 0 && (
-                          <>
-                            <Swiper
-                              spaceBetween={20}
-                              slidesPerView={1}
-                              modules={[Navigation]}
-                              className="w-full h-full"
-                              navigation={{
-                                nextEl: ".swiper-button-next",
-                                prevEl: ".swiper-button-prev",
-                              }}
-                            >
-                              {photos.map((photo, index) => (
-                                <SwiperSlide key={index}>
-                                  <Image
-                                    src={URL.createObjectURL(photo)}
-                                    className="ibg"
-                                    alt={`Uploaded image ${index + 1}`}
-                                    width={600}
-                                    height={600}
+                        {(photos && photos?.length > 0) ||
+                          (product?.photos && product?.photos.length > 0 && (
+                            <>
+                              <Swiper
+                                spaceBetween={20}
+                                slidesPerView={1}
+                                modules={[Navigation]}
+                                className="w-full h-full"
+                                navigation={{
+                                  nextEl: ".swiper-button-next",
+                                  prevEl: ".swiper-button-prev",
+                                }}
+                              >
+                                {product?.photos &&
+                                  product.photos.map((photo, index) => (
+                                    <SwiperSlide key={index}>
+                                      <Image
+                                        src={photo.url}
+                                        className="ibg"
+                                        alt={`Uploaded image ${index + 1}`}
+                                        width={600}
+                                        height={600}
+                                      />
+                                    </SwiperSlide>
+                                  ))}
+                                {photos.map((photo, index) => (
+                                  <SwiperSlide key={index}>
+                                    <Image
+                                      src={URL.createObjectURL(photo)}
+                                      className="ibg"
+                                      alt={`Uploaded image ${index + 1}`}
+                                      width={600}
+                                      height={600}
+                                    />
+                                  </SwiperSlide>
+                                ))}
+                              </Swiper>
+                              <button
+                                type="button"
+                                className="swiper-button swiper-button-prev "
+                              >
+                                <svg
+                                  width="41"
+                                  height="41"
+                                  viewBox="0 0 41 41"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <rect
+                                    x="0.5"
+                                    y="0.5"
+                                    width="40"
+                                    height="40"
+                                    rx="3.5"
+                                    fill="#1858B8"
+                                    stroke="#1858B8"
                                   />
-                                </SwiperSlide>
-                              ))}
-                            </Swiper>
-                            <button
-                              type="button"
-                              className="swiper-button swiper-button-prev "
-                            >
-                              <svg
-                                width="41"
-                                height="41"
-                                viewBox="0 0 41 41"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
+                                  <path
+                                    d="M24.5303 21.5303C24.8232 21.2374 24.8232 20.7626 24.5303 20.4697L19.7574 15.6967C19.4645 15.4038 18.9896 15.4038 18.6967 15.6967C18.4038 15.9896 18.4038 16.4645 18.6967 16.7574L22.9393 21L18.6967 25.2426C18.4038 25.5355 18.4038 26.0104 18.6967 26.3033C18.9896 26.5962 19.4645 26.5962 19.7574 26.3033L24.5303 21.5303ZM24 20.25H23V21.75H24V20.25Z"
+                                    fill="white"
+                                  />
+                                </svg>
+                              </button>
+                              <button
+                                type="button"
+                                className="swiper-button swiper-button-next "
                               >
-                                <rect
-                                  x="0.5"
-                                  y="0.5"
-                                  width="40"
-                                  height="40"
-                                  rx="3.5"
-                                  fill="#1858B8"
-                                  stroke="#1858B8"
-                                />
-                                <path
-                                  d="M24.5303 21.5303C24.8232 21.2374 24.8232 20.7626 24.5303 20.4697L19.7574 15.6967C19.4645 15.4038 18.9896 15.4038 18.6967 15.6967C18.4038 15.9896 18.4038 16.4645 18.6967 16.7574L22.9393 21L18.6967 25.2426C18.4038 25.5355 18.4038 26.0104 18.6967 26.3033C18.9896 26.5962 19.4645 26.5962 19.7574 26.3033L24.5303 21.5303ZM24 20.25H23V21.75H24V20.25Z"
-                                  fill="white"
-                                />
-                              </svg>
-                            </button>
-                            <button
-                              type="button"
-                              className="swiper-button swiper-button-next "
-                            >
-                              <svg
-                                width="41"
-                                height="41"
-                                viewBox="0 0 41 41"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <rect
-                                  x="0.5"
-                                  y="0.5"
-                                  width="40"
-                                  height="40"
-                                  rx="3.5"
-                                  fill="#1858B8"
-                                  stroke="#1858B8"
-                                />
-                                <path
-                                  d="M24.5303 21.5303C24.8232 21.2374 24.8232 20.7626 24.5303 20.4697L19.7574 15.6967C19.4645 15.4038 18.9896 15.4038 18.6967 15.6967C18.4038 15.9896 18.4038 16.4645 18.6967 16.7574L22.9393 21L18.6967 25.2426C18.4038 25.5355 18.4038 26.0104 18.6967 26.3033C18.9896 26.5962 19.4645 26.5962 19.7574 26.3033L24.5303 21.5303ZM24 20.25H23V21.75H24V20.25Z"
-                                  fill="white"
-                                />
-                              </svg>
-                            </button>
-                          </>
-                        )}
+                                <svg
+                                  width="41"
+                                  height="41"
+                                  viewBox="0 0 41 41"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <rect
+                                    x="0.5"
+                                    y="0.5"
+                                    width="40"
+                                    height="40"
+                                    rx="3.5"
+                                    fill="#1858B8"
+                                    stroke="#1858B8"
+                                  />
+                                  <path
+                                    d="M24.5303 21.5303C24.8232 21.2374 24.8232 20.7626 24.5303 20.4697L19.7574 15.6967C19.4645 15.4038 18.9896 15.4038 18.6967 15.6967C18.4038 15.9896 18.4038 16.4645 18.6967 16.7574L22.9393 21L18.6967 25.2426C18.4038 25.5355 18.4038 26.0104 18.6967 26.3033C18.9896 26.5962 19.4645 26.5962 19.7574 26.3033L24.5303 21.5303ZM24 20.25H23V21.75H24V20.25Z"
+                                    fill="white"
+                                  />
+                                </svg>
+                              </button>
+                            </>
+                          ))}
 
                         <input
                           type="file"
@@ -949,7 +941,10 @@ export default function Advertisement({ categories }: SellProps) {
                           <div className="downloads-input-body-file__image"></div>
                         </div>
                         <p className="downloads-input-body-file__text">
-                          <span>{photos?.length || 0} </span>{" "}
+                          <span>
+                            {(photos?.length || 0) +
+                              (product?.photos?.length || 0)}
+                          </span>{" "}
                           {t("photos-uploaded")}
                         </p>
                       </div>
@@ -962,7 +957,7 @@ export default function Advertisement({ categories }: SellProps) {
                         </label>
                         <button
                           type="button"
-                          onClick={(e) => setPhotos([])}
+                          onClick={handleDeletePhotos}
                           className="input-body-file__delete"
                         >
                           {t("delete")}
