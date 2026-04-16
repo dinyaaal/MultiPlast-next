f5e45dc475cb9097a5ed5ee2290a3ac01f1ffce6
dinyaaal <wdinyaaal@gmail.com>
2026-04-16 14:01:04 +0200

    Refactor BackBtn component for improved navigation handling
    - Replaced Link with useRouter for better back navigation logic.
    - Implemented handleBack function to navigate based on browser history.
    - Updated ButtonMain to use onPress instead of href for handling back navigation.

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link } from "@/i18n/routing";
+import { useRouter } from "@/i18n/routing";
 import { ButtonMain } from "./ButtonMain";
 import { ChevronLeft } from "lucide-react";
 import { useTranslations } from "next-intl";
@@ -8,8 +8,18 @@ import { useTranslations } from "next-intl";
 
 export default function BackBtn({ href }: { href: string }) {
     const t = useTranslations("Back");
+    const router = useRouter();
+
+    const handleBack = () => {
+        if (window.history.length > 1) {
+            router.back();
+        } else {
+            router.push(href);
+        }
+    };
+
     return (
-        <ButtonMain color='secondary' size={'md'} variant={'bordered'} as={Link} href={href}>
+        <ButtonMain color='secondary' size={'md'} variant={'bordered'} onPress={handleBack}>
             <ChevronLeft size={20} />
             {t(`back`)}
         </ButtonMain>
